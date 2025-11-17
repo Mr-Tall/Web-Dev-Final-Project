2ab6188cf9b017b252c77f200b8d9ec1a99bcc7d
LianaW04 <143361927+LianaW04@users.noreply.github.com>
2025-11-16 23:41:50 -0500

    add initial react scaffold with src and book data

---
+++ book-details/vite-project/src/data/book.js
@@ -0,0 +1,39 @@
+export const book = {
+    "title": "The Lord of the Rings",
+    "author": "J.R.R. Tolkien",
+    "cover": "LOTRimage.jpg",
+    "description": "The Lord of the Rings is an epic fantasy novel by J.R.R. Tolkien. It is the first volume of The Hobbit, followed by The Fellowship of the Ring, The Two Towers, and The Return of the King.",
+    "releaseDate": "1954-07-29",
+    "isbn": "978-0-544-00035-4",
+    "tags": ["Fantasy", "Adventure", "Fantasy Fiction", "Epic Fantasy"],
+    "ratings": [1,2,3,3,4,4,4,5,5,5,5,5],
+    "reviews": [
+        {
+            "author": "John Doe",
+            "rating": 5,
+            "upvotes": 10,
+            "comment": "I loved this book!"
+        },
+        {
+            "author": "Jane Doe",
+            "rating": 4,
+            "upvotes": 5,
+            "comment": "I liked this book."
+        }
+    ],
+    "availability": [
+        {
+            "library": "O'Neill",
+            "available": 3,
+            "quantity": 10,
+            "type": "Hardcover"
+        },
+        {
+            "library": "Bapst",
+            "available": 0,
+            "quantity": 5,
+            "type": "Audiobook"
+        }
+    ],
+    relatedBooks: ["The Hobbit", "The Fellowship of the Ring", "The Two Towers", "The Return of the King"]
+}
