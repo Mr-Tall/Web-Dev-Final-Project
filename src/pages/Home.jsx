abb88ac1f4c80b84dacc29ea273a262f5986baf1
Jason <jungft@bc.edu>
2025-11-19 19:07:48 -0500

    Fix: Add React import to Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import booksData from '../data/books.json'
 import SearchBar from '../components/SearchBar'
